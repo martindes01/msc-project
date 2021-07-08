d69a275d06409c4f4ad1e1d88a02b5209bf16912
Martin de Spirlet <martindes01@gmail.com>
2021-07-08 12:27:25 +0100

    Document IdentificationSummary interface
    - Add Javadoc comments to the 'IdentificationSummary' interface and its
      methods.

--- src/lib/src/main/java/com/bham/mld705/summaries/IdentificationSummary.java
+++ src/lib/src/main/java/com/bham/mld705/summaries/IdentificationSummary.java
@@ -1,7 +1,23 @@
 package com.bham.mld705.summaries;
 
+/**
+ * A hash summary of an integer multiset data stream. An object of a class that
+ * implements this interface can be queried for a hash value that represents the
+ * multiset from which it is built.
+ *
+ * @author Martin de Spirlet
+ * @see MultisetSummary
+ * @see Summary
+ */
 public interface IdentificationSummary<S extends IdentificationSummary<S>> extends MultisetSummary<S> {
 
+    /**
+     * Returns a hash value that represents the multiset from which this
+     * {@code IdentificationSummary} is built.
+     *
+     * @return a hash value that represents the multiset from which this
+     *         {@code IdentificationSummary} is built
+     */
     public abstract long getHashValue();
 
 }
