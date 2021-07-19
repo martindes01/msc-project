e6db42412f104777b867faaa50bf1b6e88128bf8
Martin de Spirlet <martindes01@gmail.com>
2021-07-19 12:20:16 +0100

    Document HashFunction interface
    - Add Javadoc comments to the 'HashFunction' functional interface and
      its method.

--- src/lib/src/main/java/com/bham/mld705/util/hashfunctions/HashFunction.java
+++ src/lib/src/main/java/com/bham/mld705/util/hashfunctions/HashFunction.java
@@ -1,8 +1,19 @@
 package com.bham.mld705.util.hashfunctions;
 
+/**
+ * A hash function that maps an integer key to an integer hash value.
+ *
+ * @author Martin de Spirlet
+ */
 @FunctionalInterface
 public interface HashFunction {
 
+    /**
+     * Maps the given key to a hash value.
+     *
+     * @param key the key to map to a hash value
+     * @return the hash value of the given key
+     */
     public abstract int apply(int key);
 
 }
