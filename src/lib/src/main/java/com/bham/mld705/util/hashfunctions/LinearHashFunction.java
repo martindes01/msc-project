8f8a5c13f872f9e1b948b3d43cc95c3bb802ed1d
Martin de Spirlet <martindes01@gmail.com>
2021-07-19 13:32:16 +0100

    Document LinearHashFunction interface
    - Add Javadoc comments to the 'LinearHashFunction' class and its
      methods.

--- src/lib/src/main/java/com/bham/mld705/util/hashfunctions/LinearHashFunction.java
+++ src/lib/src/main/java/com/bham/mld705/util/hashfunctions/LinearHashFunction.java
@@ -2,6 +2,14 @@ package com.bham.mld705.util.hashfunctions;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * A pairwise independent hash function that maps an integer key to an integer
+ * hash value. This hash function is drawn from the family of polynomials. The
+ * probability of observing any pair of hash values is uniform.
+ *
+ * @author Martin de Spirlet
+ * @see HashFunction
+ */
 public final class LinearHashFunction implements HashFunction {
 
     private static final int PARAMETER_LOWER_BOUND = 1;
@@ -11,6 +19,14 @@ public final class LinearHashFunction implements HashFunction {
     private final int PARAMETER_B;
     private final int PRIME;
 
+    /**
+     * Constructs a new {@code LinearHashFunction} with the given prime and modulus.
+     * Note that the given prime must be prime in order for the new
+     * {@code LinearHashFunction} to be pairwise independent.
+     *
+     * @param prime   the prime of the new {@code LinearHashFunction}
+     * @param modulus the modulus of the new {@code LinearHashFunction}
+     */
     public LinearHashFunction(int prime, int modulus) {
         PRIME = prime;
         MODULUS = modulus;
@@ -19,11 +35,22 @@ public final class LinearHashFunction implements HashFunction {
         PARAMETER_B = ThreadLocalRandom.current().nextInt(PARAMETER_LOWER_BOUND, PRIME);
     }
 
+    /**
+     * Maps the given key to a pairwise independent hash value.
+     *
+     * @param key the key to map to a hash value
+     * @return the hash value of the given key
+     */
     @Override
     public int apply(int key) {
         return ((PARAMETER_A * key + PARAMETER_B) % PRIME) % MODULUS;
     }
 
+    /**
+     * Returns a {@code String} that represents this {@code LinearHashFunction}.
+     *
+     * @return a {@code String} that represents this {@code LinearHashFunction}
+     */
     @Override
     public String toString() {
         return "LinearHashFunction [PRIME=" + PRIME + ", MODULUS=" + MODULUS + ", PARAMETER_A=" + PARAMETER_A
